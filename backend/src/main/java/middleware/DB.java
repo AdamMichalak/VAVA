e4c0fabe3ed064d8a1c372103160f9fc0bfe4aad
BarutJozef <71825066+BarutJozef@users.noreply.github.com>
2022-04-24 14:10:18 +0200

    browse vracia base64 image

--- backend/src/main/java/middleware/DB.java
+++ backend/src/main/java/middleware/DB.java
@@ -155,6 +155,12 @@ public class DB {
 				tmp.put("max_participants", result.getInt("max_participate"));
 				tmp.put("expiration_date", result.getDate("expiration_date"));
 				tmp.put("created_at", result.getDate("created_at"));
+
+				if (result.getBytes("title_photo") == null) {
+					tmp.put("title_photo", "");
+				} else {
+					tmp.put("title_photo", Base64.getEncoder().encodeToString(result.getBytes("title_photo")));
+				}
 				events.put(tmp);
 			}
 		}
